4b74d865a05b21107744c6831beffba1a9914465
Phil Cable <pcable@wsu.edu>
2016-04-18 13:53:04 -0700

    Hide title and slug fields in the snippet editor

--- includes/js/wsu-wpseo-metabox.js
+++ includes/js/wsu-wpseo-metabox.js
@@ -4,15 +4,9 @@
 
 	$(window).load(function() {
 
-		$( '#snippet-editor-title' ).prop({
-  			disabled: true,
-			placeholder: 'Please edit the post title.',
-			value: ''
-		});
-
-		// Hide the wpseo slug field but otherwise leave it alone
-		// because it's bound to the default WP slug field.
-		$( '#snippet-editor-slug' ).before( '<input type="text" class="snippet-editor__input" value="" placeholder="Please edit the permalink." disabled="disabled">' ).hide();
+		$( '#snippet-editor-title' ).hide();
+		$( '#snippet-editor-slug' ).hide();
+		$( '[for=snippet-editor-slug]').hide();
 
 	});
 
